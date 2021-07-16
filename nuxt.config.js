6ee1b09a4d354ee5952668f548f7b464e5c762c4
Nassos Yiannopoulos <nassosyian@gmail.com>
2021-07-16 09:21:35 +0300

    favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,11 +38,24 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: siteConfig.description || '' }
+			{ hid: 'description', name: 'description', content: siteConfig.description || '' },
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-
+			{ rel: 'icon', type: 'image/x-icon', href: '/_favicons/favicon.ico' },
+			{ rel:"apple-touch-icon", sizes:"57x57", href: "/_favicons/apple-icon-57x57.png" },
+			{ rel:"apple-touch-icon", sizes:"60x60", href: "/_favicons/apple-icon-60x60.png" },
+			{ rel:"apple-touch-icon", sizes:"72x72", href: "/_favicons/apple-icon-72x72.png" },
+			{ rel:"apple-touch-icon", sizes:"76x76", href: "/_favicons/apple-icon-76x76.png" },
+			{ rel:"apple-touch-icon", sizes:"114x114", href: "/_favicons/apple-icon-114x114.png" },
+			{ rel:"apple-touch-icon", sizes:"120x120", href: "/_favicons/apple-icon-120x120.png" },
+			{ rel:"apple-touch-icon", sizes:"144x144", href: "/_favicons/apple-icon-144x144.png" },
+			{ rel:"apple-touch-icon", sizes:"152x152", href: "/_favicons/apple-icon-152x152.png" },
+			{ rel:"apple-touch-icon", sizes:"180x180", href: "/_favicons/apple-icon-180x180.png" },
+			{ rel:"icon", type:"image/png", sizes:"192x192",  href: "/_favicons/android-icon-192x192.png" },
+			{ rel:"icon", type:"image/png", sizes:"32x32", href:"/_favicons/favicon-32x32.png" },
+			{ rel:"icon", type:"image/png", sizes:"96x96", href:"/_favicons/favicon-96x96.png" },
+			{ rel:"icon", type:"image/png", sizes:"16x16", href:"/_favicons/favicon-16x16.png" },
+			{ rel:"manifest", href:"/_favicons/manifest.json" },
 		],
 		script: [
 			// webp detection
@@ -99,7 +112,7 @@ export default {
 		// 'cookie-universal-nuxt',
 
 		'nuxt-social-meta',
-		'nuxt-rfg-icon',
+		// 'nuxt-rfg-icon', // they changed their APIs
 
 	],
 
